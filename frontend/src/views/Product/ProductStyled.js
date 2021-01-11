598d503c4f96792bf74c3a0c8eec24935d5aeb88
WooCashpirowski <cashpirowski@gmail.com>
2021-01-11 14:31:38 +0100

    product view styled

--- frontend/src/views/Product/ProductStyled.js
+++ frontend/src/views/Product/ProductStyled.js
@@ -1,9 +1,73 @@
-import styled from 'styled-components';
-import ProductView from './ProductView';
+import styled from "styled-components";
+import ProductView from "./ProductView";
 
 const ProductStyled = styled.section`
   .product-wrapper {
     display: flex;
+    align-items: flex-start;
+    .image-section {
+      flex: 4;
+      img {
+        width: 100%;
+      }
+    }
+    .middle-section {
+      flex: 5;
+      padding-right: 2rem;
+      display: flex;
+      flex-direction: column;
+      height: 100%;
+    }
+    .add-to-cart-section {
+      flex: 3;
+      p {
+        display: flex;
+        justify-content: space-between;
+        padding: 0.5rem;
+        border: 1px solid var(--dark-secondary);
+        &:first-child {
+          border-bottom: none;
+        }
+        span {
+          font-weight: 500;
+        }
+      }
+      button {
+        display: inline-block;
+        width: 100%;
+        border: none;
+        padding: 0.5rem;
+        margin-top: 0.5rem;
+        background: var(--dark-red);
+        color: white;
+        font-size: 16px;
+        cursor: pointer;
+        position: relative;
+        overflow: hidden;
+        .slide {
+          position: absolute;
+          width: 100%;
+          height: 100%;
+          top: 0;
+          left: 0;
+          transform: translateX(-100%);
+          background: var(--light-pink);
+          transition: all 0.3s ease-Out;
+        }
+        span {
+          position: relative;
+          transition: all 0.3s ease-Out;
+        }
+        &:hover {
+          .slide {
+            transform: translateX(0);
+          }
+          span {
+            color: var(--dark-secondary);
+          }
+        }
+      }
+    }
   }
 `;
 
--- frontend/src/views/Product/ProductView.js
+++ frontend/src/views/Product/ProductView.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Rating from '../../components/Rating/Rating';
-import products from '../../products';
-import { ImHome } from 'react-icons/im';
-import ProductStyled from './ProductStyled';
+import React from "react";
+import { Link } from "react-router-dom";
+import Rating from "../../components/Rating/Rating";
+import products from "../../products";
+import { ImHome } from "react-icons/im";
+import ProductStyled from "./ProductStyled";
 
 const ProductView = ({ match }) => {
   const product = products.find((p) => p._id === parseInt(match.params.id));
   return (
     <ProductStyled>
       <Link to="/" className="link-btn">
-        {' '}
+        {" "}
         <ImHome /> Wróć
       </Link>
       <div className="product-wrapper">
@@ -20,18 +20,32 @@ const ProductView = ({ match }) => {
         <div className="middle-section">
           <h1>{product.name}</h1>
           <div className="rating">
-            <Rating value={product.value} text={product.numReviews} />
+            <Rating
+              value={product.rating}
+              text={`${product.numReviews} ocen`}
+            />
           </div>
-          <h3>{product.price}</h3>
+          <h3>{product.price} zł</h3>
           <p>{product.description}</p>
         </div>
-        <div className="cart-section">
+        <div className="add-to-cart-section">
           <p>
             Cena: <span>{product.price} zł</span>
           </p>
           <p>
-            W magazynie: <span>{product.countInStock}</span>
+            Status:{" "}
+            <span>
+              {product.countInStock > 0 ? "W magazynie" : "Niedostępny"}
+            </span>
           </p>
+          <button
+            type="button"
+            disabled={product.countInStock === 0}
+            onClick={() => console.log("clicked")}
+          >
+            <div className="slide" />
+            <span>Dodaj do koszyka</span>
+          </button>
         </div>
       </div>
     </ProductStyled>
