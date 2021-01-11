62b7f31c9997e8a272b1acafa8ce0c61afbd00dc
Woo Cashpirowski <cashpirowski@gmail.com>
2021-01-11 22:13:38 +0100

    fetch data from backend

--- frontend/src/views/Home/HomeView.js
+++ frontend/src/views/Home/HomeView.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ProductCard from '../../components/ProductCard/ProductCard';
-import products from '../../products';
+import axios from 'axios';
 import { HomeStyled } from './HomeStyled';
 
 const Home = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get('/api/products');
+      setProducts(data);
+    };
+    fetchProducts();
+  }, []);
+
   return (
     <HomeStyled>
       <h1 className="section-header">Polecane</h1>
--- frontend/src/views/Product/ProductView.js
+++ frontend/src/views/Product/ProductView.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 import Rating from '../../components/Rating/Rating';
-import products from '../../products';
 import { ImHome } from 'react-icons/im';
 import ProductStyled from './ProductStyled';
 
 const ProductView = ({ match }) => {
-  const product = products.find((p) => p._id === match.params.id);
+  const [product, setProduct] = useState({});
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${match.params.id}`);
+      setProduct(data);
+    };
+    fetchProduct();
+  }, [match]);
   return (
     <ProductStyled>
       <Link to="/" className="link-btn">
