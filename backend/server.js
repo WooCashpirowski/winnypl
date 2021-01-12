9db0a0ccd7e9fc5779d984809dbf527efcbb3da8
WooCashpirowski <cashpirowski@gmail.com>
2021-01-12 11:09:40 +0100

    db. db connect, data models

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,24 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import products from './data/products.js';
+import express from "express";
+import dotenv from "dotenv";
+import colors from "colors";
+import connectDB from "./config/db.js";
+import products from "./data/products.js";
 
 dotenv.config();
 
+connectDB();
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('API śmiga...');
+app.get("/", (req, res) => {
+  res.send("API śmiga...");
 });
 
-app.get('/api/products', (req, res) => {
+app.get("/api/products", (req, res) => {
   res.json(products);
 });
 
-app.get('/api/products/:id', (req, res) => {
+app.get("/api/products/:id", (req, res) => {
   const product = products.find((p) => p._id === req.params.id);
   res.json(product);
 });
@@ -23,5 +27,5 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
-  console.log(`serwer: port ${PORT}, tryb ${process.env.NODE_ENV}`),
+  console.log(`serwer: port ${PORT}, tryb ${process.env.NODE_ENV}`.yellow.bold)
 );
