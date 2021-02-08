9e0d253c300392c64c6154f632df9292c7222cef
Woo Cashpirowski <cashpirowski@gmail.com>
2021-02-08 23:07:57 +0100

    home page css fix

--- frontend/src/views/Home/HomeStyled.js
+++ frontend/src/views/Home/HomeStyled.js
@@ -4,7 +4,7 @@ export const HomeStyled = styled.section`
   min-height: calc(100vh - 100px);
   .section.featured {
     display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+    grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
     justify-items: center;
     grid-gap: 1rem;
   }
